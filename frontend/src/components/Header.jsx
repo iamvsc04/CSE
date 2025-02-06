3bd6cff6f89e366827f36a2893bee70c5d713978
kaushik <kaushikkomaravolu@gmail.com>
2025-02-06 20:00:14 +0530

    minor bug fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 const Navbar = () => {
   return (
