6ab3a9e63c00c60d2dc6cc15ebf95a1cc6e179f0
kaushik <kaushikkomaravolu@gmail.com>
2025-02-22 06:24:10 +0530

    minor changes in footer

--- src/pages/StudentCorner/Gallery.jsx
+++ src/pages/StudentCorner/Gallery.jsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from "react";
+import "../../styles.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 const Gallery = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  return(
+    <>
+    <div>
+      <Header />
+      <Footer /> 
+    </div>     
+    </>
+  )
 };
 
 export default Gallery;
